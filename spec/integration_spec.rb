7768839b124648d6e305325bcf5373d34976d9af
Duncan Fermor <duncan.fermor@gmail.com>
2014-09-28 02:38:54 +0100

    refactored integration tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,22 +5,15 @@ require_relative '../connection'
 
 describe do
 
-  it "can detect the operating system of a switch" do
-    catos_check = State.new(message: "set length 0",
-                            decision: Proc.new { |response| !response.match(/Invalid/) })
-    ios_check   = State.new(message: "term len 0",
-                            decision: Proc.new { |response| !response.match(/Not good/) })
+  let(:catos_check)      { State.new(message: "set length 0", decision: Proc.new { |response| !response.match(/Invalid/)  }) }
+  let(:ios_check)        { State.new(message: "term len 0",   decision: Proc.new { |response| !response.match(/Not good/) }) }
+  let(:catos_first_step) { State.new }
+  let(:ios_first_step)   { State.new }
+  let(:catos_power)      { State.new(message: "show power consumption", result:  Proc.new { |response| response.match(/Power Consumption: (\d+)/).to_a[1] })  }
+  let(:ios_power)        { State.new(message: "power consumption", result:  Proc.new { |response| response.match(/Total Power Consumption: (\d+)/).to_a[1] }) }
+  let(:detection_failed) { State.new(result: Proc.new { |response| raise Exception, "can't detect switch type" }) }
 
-    catos_first_step = State.new
-    ios_first_step   = State.new
-
-    catos_power = State.new(message: "show power consumption",
-                            result:  Proc.new { |response| response.match(/Power Consumption: (\d+)/).to_a[1] })
-    ios_power   = State.new(message: "power consumption",
-                            result:  Proc.new { |response| response.match(/Total Power Consumption: (\d+)/).to_a[1] })
-
-    detection_failed = State.new result: Proc.new { |response| raise Exception, "can't detect switch type" }
-
+  before do
     catos_check.next_step   = ios_check
     catos_check.branch_step = catos_first_step
 
@@ -29,7 +22,9 @@ describe do
 
     catos_first_step.next_step = catos_power
     ios_first_step.next_step   = ios_power
+  end
 
+  it "can detect an ios switch" do
     ssh = double(:ssh)
     expect(ssh).to receive(:puts   ).with("set length 0").ordered
     expect(ssh).to receive(:waitfor).with(/prompt>/).and_return("That command was Invalid").ordered
@@ -48,4 +43,35 @@ describe do
     keith.walk
   end
 
+  it "can detect a catos switch" do
+    ssh = double(:ssh)
+    expect(ssh).to receive(:puts   ).with("set length 0").ordered
+    expect(ssh).to receive(:waitfor).with(/prompt>/).and_return("").ordered
+
+    expect(ssh).to receive(:puts   ).with("show power consumption").ordered
+    expect(ssh).to receive(:waitfor).with(/prompt>/).and_return("Power Consumption: 600W\nBreakdown: blah blah").ordered
+
+    connection = Connection.new(ssh, 'prompt>')
+    pipe = Pipe.new(connection)
+
+    keith = Keith.new(pipe, :puts_and_wait_for_prompt)
+    keith.state = catos_check
+    keith.walk
+  end
+
+  it "breaks if nothing works" do
+    ssh = double(:ssh)
+    expect(ssh).to receive(:puts   ).with("set length 0").ordered
+    expect(ssh).to receive(:waitfor).with(/prompt>/).and_return("That command was Invalid").ordered
+
+    expect(ssh).to receive(:puts   ).with("term len 0").ordered
+    expect(ssh).to receive(:waitfor).with(/prompt>/).and_return("That command was Not good").ordered
+
+    connection = Connection.new(ssh, 'prompt>')
+    pipe = Pipe.new(connection)
+
+    keith = Keith.new(pipe, :puts_and_wait_for_prompt)
+    keith.state = catos_check
+    expect { keith.walk }.to raise_error Exception, "can't detect switch type"
+  end
 end
